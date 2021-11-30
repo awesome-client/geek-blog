597d7259bd1ab96a564117f6fb64166339102e31
Do P <1589189539@qq.com>
2021-11-30 16:47:34 +0800

    store分层

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import {createStore} from 'vuex'
+import test from './modules/test'
 
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    test,
+  },
 })
